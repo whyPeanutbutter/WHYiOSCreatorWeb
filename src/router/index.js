67df4c57b9726818c499d5d2b6e957aedd440839
wuhy <91169814+whyPeanutbutter@users.noreply.github.com>
2023-06-12 19:35:28 +0800

    beibei pingyu

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 // 引入
 import { createRouter, createWebHashHistory } from 'vue-router'
+import beibei from '../view/beibei.vue'
 /* 引入首页 路由 */
 import Home from '../view/home.vue'
 import phoneHome from '../view/phoneHome.vue'
 
+
 const Router = createRouter({
     history: createWebHashHistory(),
     routes: [{
-        path: '/',
+        path: '/beibei',
+        name: 'beibei',
+        component: beibei
+
+    },{
+        path: '/',//root 目录
         name: 'home',
         component: Home
     },
@@ -15,8 +22,8 @@ const Router = createRouter({
         path: '/phoneHome',
         name: 'phoneHome',
         component: phoneHome
-
-    }]
+    }
+    ]
 })
 
 // 导出
